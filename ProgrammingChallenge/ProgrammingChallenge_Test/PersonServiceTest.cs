a4b2cc7f42990ca6d240f9bba9312cf3d65f0070
joselarian <38096636+joselarian@users.noreply.github.com>
2018-04-06 18:00:04 +1000

    Added unit test for PersonService

--- ProgrammingChallenge/ProgrammingChallenge_Test/PersonServiceTest.cs
+++ ProgrammingChallenge/ProgrammingChallenge_Test/PersonServiceTest.cs
@@ -1,14 +1,66 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using ProgrammingChallenge_Data;
+using ProgrammingChallenge_Service; 
 
 namespace ProgrammingChallenge_Test
 {
-    [TestClass]
+    [TestFixture]
     public class PersonServiceTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Mock<IPersonRepository> MockPersonRepository { get; set; }
+        private PersonService PersonService { get; set; }
+
+        [SetUp]
+        public void Setup()
         {
+            MockPersonRepository = new Mock<IPersonRepository>(); 
+        }
+
+        [Test]
+        public void CheckWhetherNullPetsAreNotIncludedInList()
+        {
+            //Arrange
+            var data = "[{\"name\":\"Steve\",\"gender\":\"Male\",\"age\":45,\r\n\"pets\":\r\nnull\r\n}]";
+            MockPersonRepository.Setup(x => x.GetPetsListData()).ReturnsAsync(data);
+            PersonService = new PersonService(MockPersonRepository.Object);
+            //Act
+            var output = PersonService.GetPersonsList();
+            //Assert
+            MockPersonRepository.Verify(x => x.GetPetsListData(), Times.Once);
+            Assert.AreEqual(output.Count, 0);
+        }
+
+        [Test]
+        public void CheckWhetherGetPersonsListMethodProduceCorrectList()
+        {
+            //Arrange
+            var data = "[{\"name\":\"Alice\",\"gender\":\"Female\",\"age\":64,\r\n\"pets\":\r\n[{\"name\":\"Simba\",\"type\":\"Cat\"},{\"name\":\"Nemo\",\"type\":\"Fish\"}]\r\n},{\"name\":\"Jennifer\",\"gender\":\"Female\",\"age\":18,\r\n\"pets\":\r\n[{\"name\":\"Garfield\",\"type\":\"Cat\"}]\r\n}]";
+            MockPersonRepository.Setup(x => x.GetPetsListData()).ReturnsAsync(data);
+            PersonService = new PersonService(MockPersonRepository.Object);
+            //Act
+            var output = PersonService.GetPersonsList();
+            //Assert
+            MockPersonRepository.Verify(x => x.GetPetsListData(), Times.Once);
+            //Female count 1
+            Assert.AreEqual(1, output.Count); 
+            Assert.AreEqual("Female",output.Keys.ToList()[0]);
+            Assert.AreEqual(2, output["Female"].Count);
+            Assert.AreEqual("Garfield", output["Female"][0].Name);
+            Assert.AreEqual("Simba", output["Female"][1].Name);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfDataReturnedFromRepositoryIsNull()
+        {
+            //Arrange
+            PersonService = new PersonService(MockPersonRepository.Object);
+            //Act
+            var ex = Assert.Throws<ArgumentNullException>(() => PersonService.GetPersonsList());
+            //Assert
+            Assert.AreNotEqual(ex, null);
         }
     }
 }
